c4dc458ac9e56469f62a1e5409bf405efb9d7bdf
shagyeong <shagyeong21@gmail.com>
2025-02-26 17:35:48 +0900

    Update: 2025-02-26

---
+++ test.c
@@ -0,0 +1,12 @@
+//1684: 같은 나머지(수학, 정수론, 유클리드 호제법)
+#include<stdio.h>
+int gcd(int a, int b);
+
+int main(void){
+    int i, j, k;
+    int n; scanf("%d", &n);
+    
+}
+int gcd(int a, int b){
+    
+}
